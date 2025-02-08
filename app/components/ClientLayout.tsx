866a482b7ac87c13f94f876a8c0d3133388d7913
RidzzBruhh <khoirdev@gmail.com>
2025-02-08 13:21:42 +0700

    update

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -5,13 +5,23 @@ import { SearchProvider } from '../context/SearchContext';
 import Sidebar from './Sidebar';
 import MainContent from './MainContent';
 import SearchModal from './SearchModal';
+import { useState } from 'react';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const handleCloseSearch = () => {
+    setIsSearchOpen(false);
+  };
+
   return (
     <SearchProvider>
       <SidebarProvider>
         <Sidebar />
-        <SearchModal />
+        <SearchModal 
+          isOpen={isSearchOpen} 
+          onClose={handleCloseSearch}
+        />
         <MainContent>{children}</MainContent>
       </SidebarProvider>
     </SearchProvider>
